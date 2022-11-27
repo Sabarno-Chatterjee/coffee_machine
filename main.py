42581a1bbb0397ecd7224067c0bed6c479865923
Sabarno Chatterjee <sabarno.05@gmail.com>
2022-11-27 10:29:24 +0530

    Edit main.py

--- main.py
+++ main.py
@@ -6,29 +6,17 @@ def take_order():
     return input("What would you like? (espresso/latte/cappuccino):\n")
 
 
-def receive_money():
+def money(order):
     print("\nPlease insert the coins.\n")
     quarters = float(input("How many quarters?: ")) * 0.25
     dimes = float(input("How many dimes?: ")) * 0.1
     nickles = float(input("How many nickles?: ")) * 0.05
     pennies = float(input("How many pennies?: ")) * 0.01
-
-    def calc():
-
-
-# print("\Here's your change.\n")
+    total_money_received = quarters + dimes + nickles + pennies
+    return total_money_received - float(menu.MENU[order]["cost"])
 
 
-# def return_money():
-
-
-# order = take_order()
-# money()
-
-print("\nPlease insert the coins.\n")
-quarters = float(input("How many quarters?: ")) * 0.25
-print(quarters)
-calc = quarters - float(menu.MENU["latte"]["cost"])
-print(calc)
-print(menu.MENU["latte"]["cost"])
-
+order = take_order()
+change = money(order)
+print(f"\nHere's your change ${change}")
+print("Have a nice day!")
