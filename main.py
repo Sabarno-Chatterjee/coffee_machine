6817de61aa15f7118fa9cc0aca6729ec34cd0668
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-05 10:18:38 +0530

    report() functional

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-
 drinks = Menu()
 available = drinks.get_items()
-
 
 machine_on = True
 
 while machine_on:
-    drink = input(f"What would you like to drink {available} .")
+    drink = input(f"What would you like to drink {available}.\n ")
     if drink == "off":
         print("Sorry, machine down for maintenance.")
         machine_on = False
+    elif drink == "report":
+        print_report = CoffeeMaker()
+        report = print_report.report()
