5539956fc4027141fe3ff334d61d89cd1a7b1056
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-05 17:27:52 +0530

    report() money is functional

--- main.py
+++ main.py
@@ -9,13 +9,10 @@ available = order.get_items()
 machine_on = True
 
 while machine_on:
+    money = MoneyMachine()
     order = Menu()
     available = order.get_items()
     order_name = input(f"What would you like to drink {available}.\n ")
-    # cost_of_drink = MenuItem(order_name)
-    # cost = cost_of_drink.cost()
-    available = order.find_drink(order_name)
-    cost_of_drink = available.cost
     if order_name == "off":
         print("Sorry, machine down for maintenance.")
         machine_on = False
@@ -23,15 +20,15 @@ while machine_on:
     elif order_name == "report":
         print_report = CoffeeMaker()
         report = print_report.report()
+        report = money.report()
         continue
+    available = order.find_drink(order_name)
+    cost_of_drink = available.cost
     drink = order.find_drink(order_name)
     resources = CoffeeMaker()
     resources_sufficient = resources.is_resource_sufficient(drink)
     if resources_sufficient:
         print(f"{order_name.capitalize()} is an excellent choice.")
-        # cost_of_drink = MenuItem(drink)
-        # cost_of_drink = cost_of_drink.cost
-        money = MoneyMachine()
         insert_coins = money.make_payment(cost_of_drink)
 
 
