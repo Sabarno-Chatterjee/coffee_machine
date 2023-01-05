4af5b79d3e858768d8466df291913be752ef1531
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-05 10:12:13 +0530

    machine_on and off are functional, able to accept choice of drink

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+drinks = Menu()
+available = drinks.get_items()
+
+
+machine_on = True
+
+while machine_on:
+    drink = input(f"What would you like to drink {available} .")
+    if drink == "off":
+        print("Sorry, machine down for maintenance.")
+        machine_on = False
