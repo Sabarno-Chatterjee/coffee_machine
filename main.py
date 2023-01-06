8b552d38c245041b8f6213587a899bd798ee7021
Sabarno Chatterjee <sabarno.05@gmail.com>
2023-01-06 09:35:05 +0530

    Edit main.py

--- main.py
+++ main.py
@@ -3,30 +3,30 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-order = Menu()
-available = order.get_items()
+menu = Menu()
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
 
 machine_on = True
-money = MoneyMachine()
-resources = CoffeeMaker()
+
 while machine_on:
 
-    available = order.get_items()
+    available = menu.get_items()
     order_name = input(f"What would you like to drink {available}.\n ")
     if order_name == "off":
         print("Sorry, machine down for maintenance.")
         machine_on = False
         continue
     elif order_name == "report":
-        report = resources.report(), money.report()
+        report = coffee_maker.report(), money_machine.report()
         continue
-    available = order.find_drink(order_name)
+    available = menu.find_drink(order_name)
     cost_of_drink = available.cost
-    drink = order.find_drink(order_name)
+    drink = menu.find_drink(order_name)
 
-    resources_sufficient = resources.is_resource_sufficient(drink)
+    resources_sufficient = coffee_maker.is_resource_sufficient(drink)
     if resources_sufficient:
         print(f"{order_name.capitalize()} is an excellent choice.")
-        insert_coins = money.make_payment(cost_of_drink)
+        insert_coins = money_machine.make_payment(cost_of_drink)
         if insert_coins:
-            coffee = resources.make_coffee(drink)
+            coffee = coffee_maker.make_coffee(drink)
